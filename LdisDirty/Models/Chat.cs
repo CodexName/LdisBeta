535d9dc02db23136c3befffba8e5df1af2f55291
illan <illan@DESKTOP-RBJB6M7>
2023-09-19 21:54:39 +0300

    ficsed

--- LdisDirty/Models/Chat.cs
+++ LdisDirty/Models/Chat.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int QuantityUser {get;set;} 
+        public int ForeiginMessageId { get; set; }
+        public Message Messages { get; set; }
         public ICollection<User> Users { get; set; }
     }
 }
--- LdisDirty/SignalREngine/ChatsHandler.cs
+++ LdisDirty/SignalREngine/ChatsHandler.cs
@@ -1,30 +1,56 @@
 ﻿using LdisDirty.DataBaseContext;
+using LdisDirty.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
 
 namespace LdisDirty.SignalREngine
 {
     public class ChatsHandler : Hub
     {
         private IHttpContextAccessor _httpContextAccess;
-        public ChatsHandler(IHttpContextAccessor contexthttp)
+        private DbContextApplication _Context;
+        public ChatsHandler(IHttpContextAccessor contexthttp,DbContextApplication context)
         {
             _httpContextAccess = contexthttp;
-        }
-        private DbContextApplication _Context;
-        public ChatsHandler(DbContextApplication context)
-        {
             _Context = context;
         }
-        public async Task Send(string message)
+        public async Task Send(string message,string group,string name)
         {
-          
-            Clients.Group("d").SendAsync("receive",message);
+            var IdGroup = _Context.Chats.AsNoTracking().FirstOrDefault(x => x.Name == group);
+
+            DateTime timesendMessage = DateTime.Now;
+            var messageInstance = new Message
+            { 
+                Text = message,
+                DateMessage = timesendMessage,
+                ForeignChatId = 
+            };
+
+            Clients.Group(group).SendAsync("receive",message,name);
         }
-        public async Task OnConnectedAsync()
+        public async Task Enter(string username,string groupname)
         {
-            var User = 
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupname);
+            await Clients.All.SendAsync("EnterInGroupNotify", $"{username} вступил в группу");
+        }
+        public override async Task OnConnectedAsync()
+        {
+            var userData = _httpContextAccess.HttpContext.Request.Cookies["userkey"];
+            var UserJsonDeserealize = JsonSerializer.Deserialize<User>(userData);
+            var User = _Context.Users.AsNoTracking().FirstOrDefault(x => x.Email == UserJsonDeserealize.Email);
+            User.Status = "Online";
+            _Context.SaveChanges();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userData = _httpContextAccess.HttpContext.Request.Cookies["userkey"];
+            var UserJsonDeserealize = JsonSerializer.Deserialize<User>(userData);
+            var User = _Context.Users.AsNoTracking().FirstOrDefault(x => x.Email == UserJsonDeserealize.Email);
+            User.Status = "Offline";
+            _Context.SaveChanges();
         }
     }
 }
